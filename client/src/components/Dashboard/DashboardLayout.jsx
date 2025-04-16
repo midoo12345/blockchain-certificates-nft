a29d5ec6a86e8bf11b93ad4ec07583a7567ba90d
“Eslam“ <aslamalmswdya@gmail.com>
2025-04-16 15:19:50 +0200

    Make Bg Dark

--- client/src/components/Dashboard/DashboardLayout.jsx
+++ client/src/components/Dashboard/DashboardLayout.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../Shared/Sidebar';
 
 const DashboardLayout = () => {
     return (
-        <div className="min-h-screen bg-gray-100">
+        <div className="min-h-screen bg-gray-950">
             <div className="sticky top-0 z-50">
                 <Navbar />
             </div>
@@ -15,7 +15,7 @@ const DashboardLayout = () => {
                     <Sidebar />
                 </div>
                 <div className="flex-grow p-6 transition-all duration-300">
-                    <div className="bg-white rounded-lg shadow-md p-6">
+                    <div className="  rounded-lg shadow-md p-6">
                         <Outlet />
                     </div>
                 </div>
